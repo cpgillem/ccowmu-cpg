a3d54884229e0168685317ec39fccdefad702cfe
Cade Gillem <cpgillem@gmail.com>
2016-03-05 12:54:15 -0500

    Add webpage and styling.

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<?php
+  require("vendor/erusev/parsedown/Parsedown.php");
+?>
+<html lang="en">
+  <head>
+    <title>cpg</title>
+
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="site.css"/>
+  </head>
+  <body>
+    <div class="container">
+      <div class="page-header">
+        <h1>CPG <small>Secretary of Computer Club</small></h1>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-12">
+          <?php
+            /* Get last thursday's minutes and a link to their page. */
+            $lastThursday = strtotime("last thursday", time());
+            $cclubPage = sprintf("https://cclub.cs.wmich.edu/minutes/%s/minutes.html", date("Y/m/d", $lastThursday));
+            $minutes = file_get_contents(sprintf("https://raw.githubusercontent.com/ccowmu/minutes/master/minutes/%s.md", date("Ymd", $lastThursday)));
+            $parsedown = new Parsedown();
+          ?>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Last Meeting's Minutes</h3>
+            </div>
+            <div class="panel-body minutes">
+              <a href="<?php echo $cclubPage; ?>">Read on the computer club site</a><hr/>
+              <?php echo $parsedown->text($minutes); ?>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Links</h3>
+            </div>
+            <div class="panel-body">
+              <p>
+                Personal Website and Blog<br/>
+                <a href="http://cadegillem.me">cadegillem.me</a>
+              </p>
+              <p>
+                Github<br/>
+                <a href="http://github.com/cpgillem">github.com/cpgillem</a>
+              </p>
+              <p>
+                Computer Club<br/>
+                <a href="http://cclub.cs.wmich.edu">cclub.cs.wmich.edu</a>
+              </p>
+              <p>
+                Twitter<br/>
+                <a href="http://twitter.com/cpgillem">@cpgillem</a>
+              </p>
+              <p>
+                LinkedIn<br/>
+                <a href="http://linkedin.com/in/cpgillem">linkedin.com/in/cpgillem</a>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-12 col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Contact Information</h3>
+            </div>
+            <div class="panel-body">
+              <p>
+                Email<br/>
+                <a href="mailto:cade.p.gillem@wmich.edu">cade.p.gillem@wmich.edu</a>
+              </p>
+
+              <p>
+                Yakko Email<br/>
+                <a href="mailto:cpg@yakko.cs.wmich.edu">cpg@yakko.cs.wmich.edu</a>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-12 col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Projects</h3>
+            </div>
+            <div class="panel-body">
+              <p>
+                Personal Site<br/>
+                <a href="http://github.com/cpgillem/cadegillem.me">Github Repo</a>
+              </p>
+              <p>
+                Western Student Association Site<br/>
+                <a href="http://westernstudentassociation.org/">Link</a>
+              </p>
+              <p>
+                ACS Site<br/>
+                <a href="http://github.com/acswmu/acs-wmu-official-site">Github Repo</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
