ff63933f1e4e39262273a1b36486201e0243e8e8
Cade Gillem <cpgillem@gmail.com>
2016-04-28 17:24:04 -0400

    Clean up some HTML spacing.

--- index.php
+++ index.php
@@ -8,14 +8,11 @@
 
     <title>cpg</title>
 
-
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-
     <!-- FONTS -->
     <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'/>
-
 
     <!-- CUSTOM CSS -->
     <link rel="stylesheet" href="site.css"/>
